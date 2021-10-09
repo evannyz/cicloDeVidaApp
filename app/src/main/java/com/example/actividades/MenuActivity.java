e115e94a219d0ee4287cb79d3948473ad82ab8c9
evans <evanny.zapata@gmail.com>
2021-10-08 22:09:15 -0300

    Se crea una app para probar logs y el ciclo de vida

---
+++ app/src/main/java/com/example/actividades/MenuActivity.java
@@ -0,0 +1,20 @@
+package com.example.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MenuActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+
+}
